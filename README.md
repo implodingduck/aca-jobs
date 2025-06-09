e43243250704bd00bce16ff9d090bc1a7a5d8c22
implodingduck <zsek320@gmail.com>
2025-06-09 17:34:12 -0500

    adjusting code

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 ## Resources
 * https://learn.microsoft.com/en-us/python/api/overview/azure/servicebus-readme?view=azure-python
 * https://github.com/microsoft/azure-container-apps/issues/502
+* https://github.com/Azure-Samples/container-apps-jobs/tree/main
--- queue/src/main.py
+++ queue/src/main.py
@@ -2,17 +2,31 @@ import os
 from azure.servicebus import ServiceBusClient
 from azure.identity import DefaultAzureCredential
 
+import logging
+import sys
+
+# handler = logging.StreamHandler(stream=sys.stdout)
+# log_fmt = logging.Formatter(fmt="%(asctime)s | %(threadName)s | %(levelname)s | %(name)s | %(message)s")
+# handler.setFormatter(log_fmt)
+# logger = logging.getLogger('azure.servicebus')
+# logger.setLevel(logging.DEBUG)
+# logger.addHandler(handler)
+
+
 FULLY_QUALIFIED_NAMESPACE = os.environ["SERVICEBUS_FULLY_QUALIFIED_NAMESPACE"]
 QUEUE_NAME = os.environ["SERVICE_BUS_QUEUE_NAME"]
 
+print(f"Fully qualified namespace: {FULLY_QUALIFIED_NAMESPACE}")
+print(f"Queue name: {QUEUE_NAME}")
+
 credential = DefaultAzureCredential()
 if credential:
-    print("I got a credential!")
+    print(f"I got a credential! {credential}")
 
-servicebus_client = ServiceBusClient(FULLY_QUALIFIED_NAMESPACE, credential)
+servicebus_client = ServiceBusClient(fully_qualified_namespace=FULLY_QUALIFIED_NAMESPACE, credential=credential)
 
 if servicebus_client:
-    print("I got a client!")
+    print(f"I got a client! {servicebus_client}")
 
 with servicebus_client:
     receiver = servicebus_client.get_queue_receiver(queue_name=QUEUE_NAME)
